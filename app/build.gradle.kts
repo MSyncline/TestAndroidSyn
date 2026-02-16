63995d0724dcb60afe754caa4cf05b328bf9d599
Ubuntu <ubuntu@ip-172-31-21-249.ec2.internal>
2026-02-16 06:37:55 +0000

    CodeFlow: fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,11 +19,11 @@ android {
     }
 
     signingConfigs {
-        getByName('debug') {
-            storeFile file('debug.keystore')
-            storePassword 'android'
-            keyAlias 'androiddebugkey'
-            keyPassword 'android'
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
         }
     }
 
