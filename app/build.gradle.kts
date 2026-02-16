ec81e5096b9601db9423cd3554c7ff46dea66397
Matthew Roberts <mattleeroberts@gmail.com>
2026-02-15 22:12:09 -0800

    debug key store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,16 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        getByName('debug') {
+            storeFile file('debug.keystore')
+            storePassword 'android'
+            keyAlias 'androiddebugkey'
+            keyPassword 'android'
+        }
+    }
+
+
     buildTypes {
         release {
             isMinifyEnabled = false
