f3b852bcef2684bff7e41efffcd724fa0749dbdf
Ubuntu <ubuntu@ip-172-31-21-249.ec2.internal>
2026-02-16 05:39:47 +0000

    CodeFlow: replace Apex in the namespace and application I...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.apex.testandroid"
+    namespace = "com.msyncline.testandroid"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.apex.testandroid"
+        applicationId = "com.msyncline.testandroid"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
